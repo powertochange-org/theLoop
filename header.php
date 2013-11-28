186fda40baa0ca7c61dfd363bd16bfd219e75de4
matthew-chell <matthew.chell@p2c.com>
2013-11-28 14:57:33 -0800

    changinh http to https

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
     <head profile="http://gmpg.org/xfn/11">    
-		<script src="http://code.jquery.com/jquery-latest.js"></script>
+		<script src="https://code.jquery.com/jquery-latest.js"></script>
         <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>" charset="<?php bloginfo('charset'); ?>" />
         <title>The Loop | An Internal Communications Blog</title>
         <?php $siteURL = get_bloginfo('url'); ?>
         <link href="<?php bloginfo('template_url'); ?>/style.css" rel="stylesheet" type="text/css" />
-        <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:700,300|Open+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:700,300|Open+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
         <?php wp_head(); ?> 
          <script type='text/javascript'>
 		 /*
