a4bf57db1f60fc48a02572b59ebaebe254abb1de
matthew-chell <matthew.chell@p2c.com>
2013-12-31 09:11:39 -0800

    remove a comment

--- public_html/wp-content/themes/carmel/footer.php
+++ public_html/wp-content/themes/carmel/footer.php
@@ -3,6 +3,6 @@
 		<div class='copy'>&copy; 2013 Power to Change Ministries. All rights reserved.</span><a class='privacy' href=''>Privacy Policy</a></div>
 	</div>
 </footer>
-<?php wp_footer();/*to get rid off "Page optimized by WP Minify WordPress Plugin" */ ?>
+<?php wp_footer(); ?>
 </body>
 </html>
