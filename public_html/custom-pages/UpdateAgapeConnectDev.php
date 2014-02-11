dc960d83c1fa1f0a76a0d120739b4ff73f353485
matthew-chell <matthew.chell@p2c.com>
2014-02-11 15:26:40 -0800

    depoly testing

--- public_html/custom-pages/UpdateAgapeConnectDev.php
+++ public_html/custom-pages/UpdateAgapeConnectDev.php
@@ -2,4 +2,13 @@
 
 echo file_get_contents('http://hebron/DeployFromGit/');
 
+include('../wp-config.php');
+			
+// Creates a connection because wp will not be active
+$con=mysqli_connect(constant("DB_HOST"),constant("DB_USER"),constant("DB_PASSWORD"),constant("DB_NAME"));
+
+$sql = "INSERT INTO  `var_dump` (`id` ,`dump` ,`time`) VALUES (NULL ,'".mysql_real_escape_string(var_export($_POST, true))."', NULL)";
+mysqli_query($con, $sql);
+mysqli_close($con);
+
 ?>
