105734ec81106adb918d27498456c6bdd5b753d8
matthew-chell <matthew.chell@p2c.com>
2014-02-11 14:22:28 -0800

    test git deploy data

--- public_html/custom-pages/UpdateAgapeConnectDev.php
+++ public_html/custom-pages/UpdateAgapeConnectDev.php
@@ -1,5 +1,14 @@
 <?php
 
-echo file_get_contents('http://hebron/DeployFromGit/');
+//echo file_get_contents('http://hebron/DeployFromGit/');
+
+include('../wp-config.php');
+			
+// Creates a connection because wp will not be active
+$con=mysqli_connect(constant("DB_HOST"),constant("DB_USER"),constant("DB_PASSWORD"),constant("DB_NAME"));
+
+$sql = "INSERT INTO  `var_dump` (`id` ,`dump` ,`time`) VALUES (NULL ,'".mysql_real_escape_string(var_export($d, true))."', NULL)";
+mysqli_query($con, $sql);
+mysqli_close($con);
 
 ?>
