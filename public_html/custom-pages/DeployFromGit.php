4e8198b069f0851d241aec73ebf37580819ebaf8
matthew-chell <matthew.chell@p2c.com>
2014-02-12 13:17:03 -0800

    working on git deploy

--- public_html/custom-pages/DeployFromGit.php
+++ public_html/custom-pages/DeployFromGit.php
@@ -10,23 +10,32 @@ $repos = array(
 	'theLoop' => array (
 		'/home/powertochange/domains/staff.powertochange.org',
 		'/home/development/domains/devstaff.powertochange.org'));
-$project = $_GET["project"];
-		
-if (bool in_array($project ,  $repos )){
-	foreach ($repos[$project] as $folder){
-		$gitCommand = "pull";
-		echo "<p>Running command <b>git " + $gitCommand + "</b> in folder <b>" + $folder + "</b></p>";
-		$output = runGitCommand($folder, $gitCommand);		
-		echo "<span style='font-family: courier new; font-size: small'>";
-		echo output.Replace(" ", "&nbsp;").Replace("\n", "<br />");
-		echo "</span>";
+
+if (array_key_exists("project", $_GET)){
+	$project = $_GET["project"];
+	if (array_key_exists($project ,  $repos )){
+		foreach ($repos[$project] as $folder){
+			$gitCommand = "pull";
+			echo "<p>Running command <b>git $gitCommand</b> in folder <b>$folder</b></p>";
+			$output = runGitCommand($folder, $gitCommand);
+			echo "<span style='font-family: courier new; font-size: small'>";
+			echo str_replace("\n", "<br />", str_replace(" ", "&nbsp", $output));
+			echo "</span>";
+		}
+	}
+	else {
+		echo "<p>Unknown project: <b>$project</b></p>";
 	}
 }
 else {
-	echo "<p>Unknown project <b>git " + project + "</b></p>";
+	echo "<p>No project given <BR><BR>Example:</p>";
+	echo "<span style='font-family: courier new; font-size: small'>";
+	echo "?project=AgapeConnectCentral";
+	echo "</span>";
 }
 
-function syscall ($folder, $gitCommand) {
+
+function runGitCommand ($folder, $gitCommand) {
     $descriptorspec = array( 1 => array('pipe', 'w') ); // stdout is a pipe that the child will write to
     $resource = proc_open("git $gitCommand", $descriptorspec, $pipes, $folder);
     if (is_resource($resource)) {
