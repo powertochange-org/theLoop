072ffb215cac409bd6c77f5f9f99926b72005145
matthew-chell <matthew.chell@p2c.com>
2014-02-12 11:23:19 -0800

    test

--- public_html/custom-pages/UpdateAgapeConnectDev.php
+++ public_html/custom-pages/UpdateAgapeConnectDev.php
@@ -1,13 +1,15 @@
 <?php
 
-echo file_get_contents('http://hebron/DeployFromGit/');
-
+echo file_get_contents('http://hebron/DeployFromGit/default.aspx');
+echo $_SERVER["QUERY_STRING"];
 include('../wp-config.php');
 			
 // Creates a connection because wp will not be active
 $con=mysqli_connect(constant("DB_HOST"),constant("DB_USER"),constant("DB_PASSWORD"),constant("DB_NAME"));
 
 $sql = "INSERT INTO  `var_dump` (`id` ,`dump` ,`time`) VALUES (NULL ,'".mysql_real_escape_string(var_export($_POST, true))."', NULL)";
+mysqli_query($con, $sql);
+$sql = "INSERT INTO  `var_dump` (`id` ,`dump` ,`time`) VALUES (NULL ,'".mysql_real_escape_string(var_export($_GET, true))."', NULL)";
 mysqli_query($con, $sql);
 mysqli_close($con);
 
