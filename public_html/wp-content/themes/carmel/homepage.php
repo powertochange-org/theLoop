9d687a9adbb3874ae5c4176d943bfbecd6b8fee0
matthew-chell <matthew.chell@p2c.com>
2014-02-28 10:52:30 -0800

    fixing errors

--- public_html/wp-content/themes/carmel/homepage.php
+++ public_html/wp-content/themes/carmel/homepage.php
@@ -29,7 +29,7 @@
 				document.getElementById('div_pic_' + select_pic).style.backgroundColor = '#ffffff';
 				select_pic = picture;
 				document.getElementById('slideshow').src = pics_array[select_pic];
-				document.getElementById('slideshow_link').href = link_array[select_pic];
+				document.getElementById('slideshow_link').href = links_array[select_pic];
 				document.getElementById('div_pic_' + select_pic).style.backgroundColor = '#fdbb30';
 			}
 			
@@ -49,8 +49,8 @@
 			}
 			?> null);
 			
-			var link_array = new Array (<?php
-			foreach($link as &$l){
+			var links_array = new Array (<?php
+			foreach($links as &$l){
 				echo " '$l',";
 			}
 			?> null);
