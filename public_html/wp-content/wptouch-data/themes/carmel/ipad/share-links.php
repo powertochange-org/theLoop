6f21a9b8abd4e89c15546be84975af0b878e1c0b
matthew-chell <matthew.chell@p2c.com>
2014-03-04 15:42:10 -0800

    phone

--- public_html/wp-content/wptouch-data/themes/carmel/ipad/share-links.php
+++
@@ -1,5 +0,0 @@
-<?php if ( classic_show_share_single() ) { ?>					
-	<div id="share-popover" class="popover-lists">
-		<!-- share box is detached from within #content and inserted here via JavaScript for performance reasons with iScroll -->
-	</div>
-<?php } ?>
