0cf844b2373425f438a7cc14262dec1248d809a5
matthew-chell <matthew.chell@p2c.com>
2014-03-06 15:55:12 -0800

    edit phone support

--- public_html/wp-content/plugins/wptouch-pro/themes/classic/iphone/forms.php
+++ public_html/wp-content/plugins/wptouch-pro/themes/classic/iphone/forms.php
@@ -5,74 +5,12 @@
 <?php wptouch_the_post(); ?>
 <?php $first++; ?>
 
+
 <div class="<?php wptouch_post_classes(); ?> rounded-corners-8px">
-
-	<?php if ( is_sticky() ) echo '<div class="sticky-pushpin"></div>'; ?>
-
-	<?php if ( classic_use_calendar_icons() || classic_use_thumbnail_icons() ) { ?>
-		<?php if ( wptouch_get_comment_count() ) { ?> 
-				<div class="comment-bubble <?php wptouch_comment_bubble_size(); ?>">
-				<?php comments_number( '0', '1', '%' ); ?>
-			</div>
-		<?php } ?>
-	<?php } ?>
-
-	<?php if ( classic_use_calendar_icons() ) { ?>
-		<?php $template = locate_template( 'calendar-icons.php' ); ?>	
-		<?php include( $template ); ?>
-	<?php } elseif ( classic_use_thumbnail_icons() ) { ?>
-		<?php $template = locate_template( 'thumbnails.php' ); ?>
-		<?php include( $template ); ?>
-	<?php } ?>		
-	<?php if ( !classic_mobile_excerpts_open() ) { ?>
-		<a href="#" rel="<?php the_ID(); ?>" class="excerpt-button no-ajax"></a>	
-	<?php } ?>
 	<h2><a href="<?php wptouch_the_permalink(); ?>"><?php wptouch_the_title(); ?></a></h2>
-
-	<div class="date-author-wrap">
-		<?php if ( !classic_use_calendar_icons() && classic_show_date_in_posts() ) { ?>
-			<div class="<?php wptouch_date_classes(); ?>">
-				<?php wptouch_the_time( 'F jS, Y' ); ?>
-			</div>	
-		<?php } ?>		
-		<?php if ( classic_show_author_in_posts() ) { ?>
-			<div class="post-author">
-				<?php echo sprintf( __( 'by %s', 'wptouch-pro' ), get_the_author() ); ?> 
-			</div>
-		<?php } ?>
-	</div>
-	
-	<?php if ( classic_should_show_taxonomy() ) { ?>
-		<?php if ( classic_has_custom_taxonomy() ) { ?>
-			<?php $custom_tax = classic_get_custom_taxonomy(); ?>
-			<?php if ( $custom_tax && count( $custom_tax ) ) { ?>
-				<?php foreach( $custom_tax as $tax_name => $contents ) { ?>
-					<div class="tags-and-categories">
-						<?php echo $tax_name . ': '; ?>
-						<?php $tax_array = array(); ?>
-						<?php foreach( $contents as $term ) { ?>
-							<?php $tax_array[] = '<a href="' . $term->link . '">' . $term->name . '</a>'; ?>
-						<?php } ?>
-						<?php echo implode( ', ', $tax_array ); ?>
-					</div>
-				<?php } ?>
-			<?php } ?>			
-		<?php } else { ?>
-			<?php if ( wptouch_has_tags() && classic_show_tags_in_posts() ) { ?>
-				<div class="tags-and-categories">
-					<?php _e( "Tags", "wptouch-pro" ); ?>: <?php wptouch_the_tags(); ?>
-				</div>
-			<?php } ?>
-			<?php if ( wptouch_has_categories() && classic_show_categories_in_posts() ) { ?>
-				<div class="tags-and-categories">
-					<?php _e( "Categories", "wptouch-pro" ); ?>: <?php wptouch_the_categories(); ?>
-				</div>
-			<?php } ?>	
-		<?php } ?>
-	<?php } ?>
-			
+</div>
+<div class="<?php wptouch_post_classes(); ?> rounded-corners-8px">
 	<div class="<?php wptouch_content_classes(); ?> ">
-		blah-one
 		<?php $parts = explode('<table>', get_the_content());
 			echo $parts[0].'<table>';
 			$cells = explode('<td>', $parts[1]);
@@ -81,8 +19,6 @@
 				echo '<tr><td style="border: solid 1px #000000;border-radius: 10px;padding: 10px;">'.$content[0].'</td></tr>';
 			} ?>
 			</table>
-		<a href="<?php wptouch_the_permalink(); ?>#comments" class="read-entry"><?php _e( "Comment On This Article", "wptouch-pro" ); ?></a>				
-
 	</div>
 
 </div><!-- .wptouch_posts_classes() -->
