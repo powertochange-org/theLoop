a575e77559c80f7f8b8250d39fd3e4d1386aaeb5
matthew-chell <matthew.chell@p2c.com>
2014-07-03 12:19:33 -0700

    working on email signature

--- public_html/wp-content/themes/apps/email-signature.php
+++ public_html/wp-content/themes/apps/email-signature.php
@@ -10,33 +10,32 @@ $delimiter = '&#x2e;';
 
 $current_user = wp_get_current_user();
 $user = $wpdb->get_row("SELECT * FROM employee WHERE user_login = '" . $current_user->user_login . "'");
-$phone = $wpdb->get_row("SELECT CONCAT(
-		IFNULL(`country_phone_code`, '1'), $delimiter, 
-		`area_code`, $delimiter, 
-		`contact_number`,
-		IFNULL(CONCAT($delimiter, `extension`),'')) as number
+$sql = "SELECT CONCAT(
+		IFNULL(`country_phone_code`, '1'), '$delimiter', 
+		`area_code`, '$delimiter', 
+		REPLACE(`contact_number`,'-', '$delimiter'),
+		IFNULL(CONCAT('$delimiter', `extension`),'')) as number
 			FROM phone_number 
 			WHERE 
-				`employee_id` = $user->employee_id and 
-				`is_ministry = 1 and 
+				`employee_id` = '$user->external_id' and 
 				`is_ministry` = 1 and 
 				`phone_type` = 'BUS' 
 			ORDER BY `is_primary` DESC
-			LIMIT 0,1");
+			LIMIT 0,1";
+//echo $sql;
+$phone = $wpdb->get_row($sql);
 $cell = $wpdb->get_row("SELECT CONCAT(
-		IFNULL(`country_phone_code`, '1'), $delimiter, 
-		`area_code`, $delimiter, 
-		`contact_number`,
-		IFNULL(CONCAT($delimiter, `extension`),'')) as number
+		IFNULL(`country_phone_code`, '1'), '$delimiter', 
+		`area_code`, '$delimiter', 
+		REPLACE(`contact_number`,'-', '$delimiter'),
+		IFNULL(CONCAT('$delimiter', `extension`),'')) as number
 			FROM phone_number 
 			WHERE 
-				`employee_id` = $user->employee_id and 
-				`is_ministry = 1 and 
+				`employee_id` = '$user->external_id' and
 				`is_ministry` = 1 and 
 				`phone_type` = 'CELL' 
 			ORDER BY `is_primary` DESC
 			LIMIT 0,1");
-
 
 $division = array( 'Athletes in Action' => array('', ''),
 	'Breakthrough Prayer Ministry' =>  array('', ''),
@@ -49,7 +48,7 @@ $division = array( 'Athletes in Action' => array('', ''),
 	'Finance' =>  array('', ''),
 	'GAiN' =>  array('', ''),
 	'Human Resources' =>  array('', ''),
-	'Information Technology' =>  array('', ''),
+	'Information Technology' =>  array('2014/07/Advancement-Email.png', 'http://powertochange.org/'),
 	'International' =>  array('', ''),
 	'LeaderImpact' =>  array('', ''),
 	'Ministries Office' =>  array('', ''),
@@ -70,47 +69,59 @@ get_header(); ?>
 	<hr>
     <div id="content-left">
 	<div id="main-content">
-		<script src="https://code.jquery.com/jquery-latest.js"></script>
-		
-		<label for='name'>Name:</label><input type='text' id='name' onchange='refreshSignature();' value='<?php echo "$user->first_name $user->last_name"?>'/>
-		<label for='phone'>Phone:</label><input type='text' id='phone' onchange='refreshSignature();' value='<?php echo "$phone[number]"?>'/>
-		<label for='cell'>Cell:</label><input type='text' id='cell' onchange='refreshSignature();' value='<?php echo "$cell[number]"?>'/>
-		<button type="button" onclick='refreshSignature();'>Click Me!</button>
-		<div id='preview'></div>
-		<textarea id='code' readonly></textarea>
+		<style type='text/css'>
+		.resetCSS *{
+			border:none;
+			padding:0;
+		}
+
+		.resetCSS{
+			border:solid black 1px;
+			padding:5px;
+			margin-top:5px;
+			margin-bottom:5px;
+		}
+		</style>
+		<table>
+		<tr><td><label for='name'>Name:</label></td><td><input type='text' id='name' onpaste='refreshSignature();' onkeyup='refreshSignature();' onchange='refreshSignature();' value='<?php echo "$user->first_name $user->last_name"?>'/></td></tr>
+		<tr><td><label for='phone'>Phone:</label></td><td><input type='text' id='phone' onpaste='refreshSignature();' onkeyup='refreshSignature();' onchange='refreshSignature();' value='<?php if($phone != null){echo $phone->number;} ?>'/></td></tr>
+		<tr><td><label for='cell'>Cell:</label></td><td><input type='text' id='cell'  onpaste='refreshSignature();' onkeyup='refreshSignature();' onchange='refreshSignature();' value='<?php if($cell != null){echo $cell->number;} ?>'/></td></tr>
+		</table>
+		<div class="resetCSS" id='preview'></div>
+		<textarea style='width:100%;height:200px;' id='code' readonly></textarea>
 		 
 		
 		<script type="text/javascript">
 			function refreshSignature(){
-				var signature = '<table style="font-family:verdana,sans-serif;color:#444444;width:1000px;min-width:1000px;" cellspacing="0" cellpadding="0">' +
-					'<tr style="font-family:verdana,sans-serif;font-size: 10pt;color:#231f20;font-weight: bold;margin-bottom:3px;border-top:1px solid #c0c0c0;padding-top:15px;display:inline-block">' +
-					'<td style="font-family:verdana,sans-serif;" >' +
-						document.getElementById('name').value + '</td>' +
-					'</tr>' +
-					'<tr style="font-family:verdana,sans-serif;font-size: 11px;height:18px;">' +
-					'<td style="font-family:verdana,sans-serif;" ><?php echo "$user->role_title" ?><span style='color:#c0c0c0;'>|</span><?php echo "$user->ministry" ?></td>' +
-					'</tr>' +
-					'<tr style="font-family:verdana,sans-serif;font-size: 11px;">' +
-					'<td style="font-family:verdana,sans-serif;" >T.&nbsp;<a style="text-decoration:none;color:#444444;">';
+				var signature = '<table style="border:none;font-family:verdana,sans-serif;color:#444444;width:1000px;min-width:1000px;" cellspacing="0" cellpadding="0">\n' +
+					'<tr style="font-family:verdana,sans-serif;font-size: 10pt;color:#231f20;font-weight: bold;margin-bottom:3px;border-top:1px solid #c0c0c0;padding-top:15px;display:inline-block">\n' +
+					'<td style="font-family:verdana,sans-serif;" >\n' +
+						document.getElementById('name').value + '</td>\n' +
+					'</tr>\n' +
+					'<tr style="font-family:verdana,sans-serif;font-size: 11px;height:18px;">\n' +
+					'<td style="font-family:verdana,sans-serif;" ><?php echo "$user->role_title" ?><span style="color:#c0c0c0;">|</span><?php echo "$user->ministry" ?></td>\n' +
+					'</tr>\n' +
+					'<tr style="font-family:verdana,sans-serif;font-size: 11px;">\n' +
+					'<td style="font-family:verdana,sans-serif;" >T.&nbsp;<a style="text-decoration:none;color:#444444;">\n';
 				var phone = document.getElementById('phone').value;
 				if (phone.trim() == ""){
-					signature += "<?php echo "1$delimiter604$delimiter514$delimiter2000" ?>";
+					signature += "<?php echo "1".$delimiter."604".$delimiter."514".$delimiter."2000" ?>\n";
 				}
 				else {
-					signature += phone;
+					signature += phone + '\n';
 				}
-				signature += "</a>&nbsp;<span style='color:#c0c0c0;'>|</span>&nbsp;";
+				signature += "</a>&nbsp;<span style='color:#c0c0c0;'>|</span>&nbsp;\n";
 				var cell = document.getElementById('cell').value;
 				if (cell.trim() == ""){
-					signature += 'Toll&nbsp;Free:&nbsp;<a style="text-decoration:none;color:#444444;">1&#x2e;855<?php echo "$delimiter" ?>722&#x2e;4483';
+					signature += 'Toll&nbsp;Free:&nbsp;<a style="text-decoration:none;color:#444444;"><?php echo "1".$delimiter."855".$delimiter."722".$delimiter."4483" ?>\n';
 				}
 				else {
-					signature += 'Cell:&nbsp;<a style="text-decoration:none;color:#444444;">' + cell;
+					signature += 'C:&nbsp;<a style="text-decoration:none;color:#444444;">' + cell + '\n';
 				}
-				signature += '</a></td></tr>' +
-					'<tr style="font-family:verdana,sans-serif;">' +
-					'<td style="font-family:verdana,sans-serif;" ><a href="http://powertochange.org/" target="_blank"><img src="http://powertochange.com/wp-content/uploads/2014/07/P2C-Logo-Email.png" height="80"  /></a><a href="<?php echo $division[$user->ministry][1]?>" target="_blank"><img src="http://powertochange.com/wp-content/uploads/' +
-					'<?php echo $division[$user->ministry][0]?>" height="80"  /></a></td></tr></table>';
+				signature += '</a></td></tr>\n' +
+					'<tr style="font-family:verdana,sans-serif;">\n' +
+					'<td style="font-family:verdana,sans-serif;" ><a href="http://powertochange.org/" target="_blank"><img src="http://powertochange.com/wp-content/uploads/2014/07/P2C-Logo-Email.png" height="80"  /></a><a href="<?php echo $division[$user->ministry][1]?>" target="_blank"><img src="http://powertochange.com/wp-content/uploads/\n' +
+					'<?php echo $division[$user->ministry][0]?>" height="80"  /></a></td></tr></table>\n';
 				document.getElementById('preview').innerHTML = signature;
 				document.getElementById('code').innerHTML = signature;
 
@@ -118,7 +129,7 @@ get_header(); ?>
 			
 			window.onload = refreshSignature;
 		</script>
-	</div>
+	</div></div>
     <div id="content-right"><?php get_sidebar(''); ?></div><div style='clear:both;'></div>
 </div>
 <!--content end-->
