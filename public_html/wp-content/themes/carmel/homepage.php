fd19a3b1c05bcb279ed52ead2c905f20f5e12fa9
kevinbrink <Kevin@Kevins-MacBook-Pro.local>
2014-07-04 09:00:33 -0400

    Merge branch 'master' into dev
    Updating dev with latest changes to master

--- public_html/wp-content/themes/carmel/homepage.php
+++ public_html/wp-content/themes/carmel/homepage.php
@@ -8,8 +8,8 @@
 	<div style='position:relative; top:-43px;'>
 		<?php 
 			$folder = '/wp-content/uploads/slides/';
-			$pictures = array($folder.'cycling_4_water_banner.png',		$folder.'Staff_Conference_Loop_Banner.png',	$folder.'RBC.jpg');
-			$links    = array('/all-staff/cycling-4-water-needs-your-help/',		'http://staffconference.com/',			'/corporate-discounts/discounts-across-canada/rbc-group-banking/');
+			$pictures = array($folder.'Staff_Conference_Loop_Banner.png', $folder.'RBC.jpg', $folder.'RRSP.jpg', $folder.'Branding.jpg');
+			$links    = array('http://staffconference.com/', '/corporate-discounts/discounts-across-canada/rbc-group-banking/', 		'/wp-content/uploads/2013/06/Personality-Questionnaire.pdf', '/forms-information/ptc-branding-conference-resources/');
 		?>
 		<a id='slideshow_link' target="_blank">
 		<?php foreach($pictures as $pic=>$src){
