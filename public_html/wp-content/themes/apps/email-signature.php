b6b74bf3a01bd44a5f958732f5c62ac2b4e976ef
matthew-chell <matthew.chell@p2c.com>
2014-07-15 16:35:38 -0700

    signature changing colour boxes

--- public_html/wp-content/themes/apps/email-signature.php
+++ public_html/wp-content/themes/apps/email-signature.php
@@ -91,14 +91,34 @@ get_header(); ?>
 			margin-top:5px;
 			margin-bottom:5px;
 		}
+		
+		#inputTable table {
+			border: 1px solid #F7941D;
+		}
+
+		#inputTable tr {
+			border-top: 1px solid #F7941D;
+		}
+
+		#inputTable tr + tr {
+			border-top: 1px solid black;
+		}
+
+		#inputTable td {
+			border-left: 1px solid #F7941D;
+		}
+
+		#inputTable td + td {
+			border-left: 1px solid black;
+		}
 		</style>
-		<table style="border: 2px red solid;">
+		<table id="inputTable">
 		<tr><td><label for='name'>Name:</label></td><td><input type='text' id='name' onpaste='refreshSignature();' onkeyup='refreshSignature();' onchange='refreshSignature();' value='<?php echo "$user->first_name $user->last_name"?>'/></td></tr>
 		<tr><td><label for='phone'>Phone:</label></td><td><input type='text' id='phone' onpaste='refreshSignature();' onkeyup='refreshSignature();' onchange='refreshSignature();' value='<?php if($phone != null){echo $phone->number;} ?>'/></td></tr>
 		<tr><td><label for='cell'>Cell:</label></td><td><input type='text' id='cell'  onpaste='refreshSignature();' onkeyup='refreshSignature();' onchange='refreshSignature();' value='<?php if($cell != null){echo $cell->number;} ?>'/></td></tr>
 		<tr><td><label for='ministry'>Ministry/Department:</label></td><td><input type='text' id='ministry' onpaste='refreshSignature();' onkeyup='refreshSignature();' onchange='refreshSignature();' value='<?php if($user->ministry == 'Development'){ echo 'Advancement';} else {echo $user->ministry;} ?>'/></td></tr>
 		</table>
-		<div  style="border: 2px blue solid;padding:50px;" class="resetCSS" id='preview'></div>
+		<div  style="border: 1px #0079C1 solid;padding:50px;" class="resetCSS" id='preview'></div>
 		<textarea style='width:100%;height:200px;display:none;' id='code' readonly></textarea>
 		 
 		
