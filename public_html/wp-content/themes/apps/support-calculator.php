867a5c287ff1a3aa1a6dbc392939050f33f8959f
matthew-chell <matthew.chell@p2c.com>
2014-07-21 15:48:18 -0700

    support calculator: download form open in new tab

--- public_html/wp-content/themes/apps/support-calculator.php
+++ public_html/wp-content/themes/apps/support-calculator.php
@@ -99,7 +99,12 @@ function generate_pdf(){
 				}
 				$border = 0;
 				$pdf->Cell($labelBotMaxWidth, 6,  $label[$i], $border, 0, 'L', $fill);
-				$pdf->Cell($dataMaxWidth, 6,  $data[$i], $border, 0, 'R', $fill);
+				if ($i == 25){
+					$pdf->Cell($dataMaxWidth, 6,  ($data[$i] * 100).'%', $border, 0, 'R', $fill);
+				}
+				else {
+					$pdf->Cell($dataMaxWidth, 6,  $data[$i], $border, 0, 'R', $fill);
+				}
 			}
 			
 			if (($i + 1) % 2 == 0 or $i > 7){
@@ -549,6 +554,7 @@ include('functions/js_functions.php'); ?>
 		
 		function downloadData(){
 			document.getElementById("download").value=getDownloadData();
+			document.getElementById("downloadDataForm").target = '_blank';
 			document.getElementById("downloadDataForm").submit();
 		}
 		
