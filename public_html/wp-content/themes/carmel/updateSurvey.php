3d849571e0899ddd0085e336171aed88e33231b3
kevinbrink <Kevin@Kevins-MacBook-Pro.local>
2014-07-31 15:23:54 -0400

    Merge branch 'dev'
    Close security hole in survey by using current user's credentials

--- public_html/wp-content/themes/carmel/updateSurvey.php
+++ public_html/wp-content/themes/carmel/updateSurvey.php
@@ -1,6 +1,13 @@
 <?php
-// Get the blog header, which gives us access to wordpres functions
+// Get the blog header, which gives us access to wordpress functions
 require('../../../wp-blog-header.php');
-// All this does is update the user_meta with the last time they dealt with a survey
-update_user_meta($_POST['user_id'], 'last_survey_date', $date = date('m/d/Y', time())); 
+
+// Check to make sure the user is logged in
+if (is_user_logged_in()) {
+    // Get the current user
+    $current_user = wp_get_current_user();
+    // All this does is update the user_meta with the last time they dealt with a survey
+    update_user_meta($current_user->ID, 'last_survey_date', $date = date('m/d/Y', time())); 
+}
+
 ?>
