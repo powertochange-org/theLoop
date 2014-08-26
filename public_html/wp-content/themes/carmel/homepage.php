d94dadfa0464d4fe8d3234a03e09a78a9db447ca
matthew-chell <matthew.chell@p2c.com>
2014-08-26 10:44:54 -0700

    changing banner

--- public_html/wp-content/themes/carmel/homepage.php
+++ public_html/wp-content/themes/carmel/homepage.php
@@ -8,8 +8,8 @@
 	<div style='position:relative; top:-43px;'>
 		<?php 
 			$folder = '/wp-content/uploads/slides/';
-			$pictures = array($folder.'SES_Banner_Loop.jpg', $folder.'Summer.jpg', $folder.'RBC.jpg', $folder.'Health_Wellness.jpg', $folder.'RRSP.jpg', $folder.'Branding.jpg');
-			$links    = array('/training-development/staff-engagement/staff-engagement-survey-2014/', '/all-staff/summer-fun/', '/corporate-discounts/discounts-across-canada/rbc-group-banking/', '/training-development/healthwellness/', '/wp-content/uploads/2013/06/Personality-Questionnaire.pdf', '/forms-information/ptc-branding-conference-resources/');
+			$pictures = array($folder.'SES_Banner_Loop.jpg', $folder.'RBC.jpg', $folder.'Health_Wellness.jpg', $folder.'RRSP.jpg', $folder.'Branding.jpg');
+			$links    = array('/training-development/staff-engagement/staff-engagement-survey-2014/', '/corporate-discounts/discounts-across-canada/rbc-group-banking/', '/training-development/healthwellness/', '/wp-content/uploads/2013/06/Personality-Questionnaire.pdf', '/forms-information/ptc-branding-conference-resources/');
 		?>
 		<a id='slideshow_link' target="_blank">
 		<?php foreach($pictures as $pic=>$src){
