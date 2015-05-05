bd21184273ac387a8ecf7a41f3d70b57aa3ace61
trinitystu <cameron.weir@mytwu.ca>
2015-05-05 09:04:31 -0700

    changed getStaffAppsButton to getStaffAppsButtonResponsive in header.php

--- public_html/wp-content/themes/carmel/header.php
+++ public_html/wp-content/themes/carmel/header.php
@@ -64,7 +64,7 @@ if (get_theme_mod('survey_active')) {
         jQuery(document).ready(function(){
           $.ajax({
             type: "POST",
-            url: "https://apps.powertochange.org/DesktopModules/AgapeConnect/StaffRmb/WebService.asmx/getStaffAppsButton",
+            url: "https://apps.powertochange.org/DesktopModules/AgapeConnect/StaffRmb/WebService.asmx/getStaffAppsButtonResponsive",
             success: function (data) {
                 $('#staffAppButtonPlaceholder').replaceWith(data);
             }
