76491f3e5a4b19c9d2dd2f0c780b73833d867be3
trinitystu <cameron.weir@mytwu.ca>
2015-05-07 14:41:27 -0700

    added the newest version of the sidebar to the other pages (not just the home page, with some other minor improvements

--- public_html/wp-content/themes/carmel/footer.php
+++ public_html/wp-content/themes/carmel/footer.php
@@ -1,6 +1,6 @@
 <footer>
 	<div class='bottom'>
-		<div class='copy'>&copy; 2013 Power to Change Ministries. All rights reserved.</span><a class='privacy' href=''>Privacy Policy</a></div>
+		<div class='copy'>&copy; 2015 Power to Change Ministries. All rights reserved.</span><a class='privacy' href=''>Privacy Policy</a></div>
 	</div>
 </footer>
 <?php wp_footer(); ?>
--- public_html/wp-content/themes/carmel/homepage.php
+++ public_html/wp-content/themes/carmel/homepage.php
@@ -235,21 +235,21 @@
 <div class='middle'>
 	<img class='logo' src='/wp-content/themes/carmel/img/footer-logo.png' /> <!-- change?-->
 	<img class='cluster' src='/wp-content/themes/carmel/img/Brand_Cluster_2_Line_New_Footer.png'  usemap="#clustermap" />
-	<!-- <map name="clustermap">
-		<!-- todo change?
-	  <area shape="rect" coords="0,0,115,50" href="/ministries/aia/" />
-	  <area shape="rect" coords="115,0,250,50" href="/ministries/students/" />
-	  <area shape="rect" coords="250,0,330,50" href="/ministries/gain/" />
-	  <area shape="rect" coords="330,0,470,50" href="/ministries/fl/" />
-	  <area shape="rect" coords="470,0,600,50" href="/ministries/tm/" />
-	  <area shape="rect" coords="600,0,729,50" href="/ministries/li/" />
-	  <area shape="rect" coords="0,50,125,95" href="/ministries/cs/" />
-	  <area shape="rect" coords="125,50,225,95" href="/ministries/drime/" />
-	  <area shape="rect" coords="225,50,310,95" href="/ministries/jfs/" />
-	  <area shape="rect" coords="310,50,400,95" href="/ministries/tl/" />
-	  <area shape="rect" coords="400,50,530,95" href="/ministries/icn/" />
-	  <area shape="rect" coords="530,50,600,95" href="/ministries/ce/" />
-	  <area shape="rect" coords="600,50,729,95" href="/ministries/btp/" />
-	</map> -->
+	<map name="clustermap">
+		<!-- todo change? -->
+	    <area shape="rect" coords="0,0,115,50" href="/ministries/aia/" />
+	    <area shape="rect" coords="115,0,250,50" href="/ministries/students/" />
+	    <area shape="rect" coords="250,0,330,50" href="/ministries/gain/" />
+	    <area shape="rect" coords="330,0,470,50" href="/ministries/fl/" />
+	    <area shape="rect" coords="470,0,600,50" href="/ministries/tm/" />
+	    <area shape="rect" coords="600,0,729,50" href="/ministries/li/" />
+	    <area shape="rect" coords="0,50,125,95" href="/ministries/cs/" />
+	    <area shape="rect" coords="125,50,225,95" href="/ministries/drime/" />
+	    <area shape="rect" coords="225,50,310,95" href="/ministries/jfs/" />
+	    <area shape="rect" coords="310,50,400,95" href="/ministries/tl/" />
+	    <area shape="rect" coords="400,50,530,95" href="/ministries/icn/" />
+	    <area shape="rect" coords="530,50,600,95" href="/ministries/ce/" />
+	    <area shape="rect" coords="600,50,729,95" href="/ministries/btp/" />
+	</map>
 </div>	
 <?php get_footer(); ?>
