c36ffd7e97526a519c89fedd7c6e7e1873b5bb02
Jason Brink <jason.brink@powertochange.org>
2015-09-10 15:26:17 -0700

    Rename files that are not directly related to MissionHub to be the more generic "P2CS Stats Reports". Deleted unnecessary file load_page.php

--- public_html/wp-content/themes/apps/functions.php
+++ public_html/wp-content/themes/apps/functions.php
@@ -2,6 +2,6 @@
 /* Since Apps is a child theme of Carmel, this functions.php will get used in addition to the one in the carmel folder */
 
 /* Register Ajax handlers for MissionHub stats reporting */
-include_once('missionhubstats/missionhubstats-include.php');
+include_once('p2cs-stats-reports/p2cs-stats-reports-include.php');
 
 ?>
