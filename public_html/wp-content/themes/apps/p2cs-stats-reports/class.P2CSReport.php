cbef5f507f2a08ecfc95abe2e22b0bba5b151db3
Jason Brink <jason.brink@powertochange.org>
2015-09-14 14:51:46 -0700

    Added some better code comments, including instructions on how to add a new report to the stats reporting system.

--- public_html/wp-content/themes/apps/p2cs-stats-reports/class.P2CSReport.php
+++ public_html/wp-content/themes/apps/p2cs-stats-reports/class.P2CSReport.php
@@ -10,9 +10,19 @@
  *
  * Author:		Jason Brink
  *
+ * If you want to add a new report to the stats reporting system, you will need to add it to the 
+ * $reportList collection below, so that it is recognized as a valid report. However, you shouldn't
+ * need to make any other changes to this file.
+ *
  ***************************************************************************************************/ 
+ 
 class P2CSReport {
-	// Provide a central way to determine if a report name is valid, and what filename and class name it maps to
+	/***********************************************************************************************
+	* This static (class) variable allows code that works with reports to know if a particular
+	* report is valid, and where to find it.
+	*
+	* If you create a new report, you must add it in this list.
+	***********************************************************************************************/
 	public static $reportList = array(
 			"missionhubengagement" => array(
 				"fileName" => "class.P2CSMissionHubEngagementReport.php", 
@@ -30,22 +40,43 @@ class P2CSReport {
 				"fileName" => "class.P2CSEventBriteReport.php", 
 				"className" => "P2CSEventBriteReport"),
 		);
-	
-	// Return whether the report has parameters that should be collected or not
+
+		
+	/* 
+	 * Return whether the report has parameters that should be collected or not
+	 */
 	public function hasParameters() {
 		return false;
 	}
+
 	
-	// Render the HTML controls necessary to collect any parameters required for 
+	/*
+	 * Render the HTML controls necessary to collect any parameters required for the selected
+	 * report. This function should not render a <form> tag, as the items it renders will
+	 * already be embedded inside a <form> tag. When the user clicks the "Generate Report"
+	 * button, all the data in the form will be collected and sent by an Ajax POST request
+	 * to the server, which will call the renderHTMLReport function below.
+	 */
 	public function renderParameters() {
 		return "";
 	}
 	
-	// Render the HTMl version of the report
+	
+	/*
+	 * Render the HTML version of the report. If this report has parameters, they will be
+	 * collected first before this function is called. The $postData will contain the POST
+	 * information assembled from any parameters the report might have set up in
+	 * renderParameters()
+	 */
 	public function renderHTMLReport($postData) {
 		return "";
 	}
+
 	
+	/*
+	 * This helper function can be used to quickly and easily convert a result set from
+	 * a MySQL database query (through a WPDB object) into an HTML table
+	 */
 	protected function convertWPDBResultToHTMLTable($wpdbResult) {
 		$result = "<table>";
 		$firstRow = true;
