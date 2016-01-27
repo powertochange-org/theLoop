bdfe2a574e9a4fd72d96bcff7e5b175f0403a1ea
Gerald Becker <gerald.becker@p2c.com>
2016-01-27 11:11:41 -0800

    Fixed the hardcoded value. It now looks for the id in the db.

--- public_html/wp-content/themes/carmel/wikimenu.php
+++ public_html/wp-content/themes/carmel/wikimenu.php
@@ -1,6 +1,19 @@
 <div id="wiki-nav-bar">
     <a href="/kb/" class="wiki-nav-links">Knowledge Base Home</a>
-    <?php if (current_user_can('edit_wiki', '17326')) { ?>
-    <a href="/kb/articles/?action=edit&eaction=create" class="wiki-nav-links">Create New Knowledge Base Article</a>
-    <?php } ?>
+    <?php 
+    //Find out the ID of the articles page and check if the user has access to create brand new wiki posts
+    global $wpdb;
+    $values = array();
+    
+    $sql = "SELECT ID
+            FROM wp_posts
+            WHERE post_name = 'articles' AND post_type = 'incsub_wiki'";
+    $result = $wpdb->get_results($sql, ARRAY_A);
+    if($result[0]['ID'] != NULL) {
+        if (current_user_can('edit_wiki', $result[0]['ID'])) { ?>
+        <a href="/kb/articles/?action=edit&eaction=create" class="wiki-nav-links">Create New Knowledge Base Article</a>
+    <?php 
+        } 
+     }?>
+    
 </div>
