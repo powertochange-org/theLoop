0cbc9b85965277863e5f6e04ba7e5d3983eefa1f
Jason Brink <jason.brink@powertochange.org>
2016-03-10 16:01:51 -0800

    Tweaks for Wiki

--- public_html/wp-content/themes/carmel/archive-incsub_wiki.php
+++ public_html/wp-content/themes/carmel/archive-incsub_wiki.php
@@ -25,7 +25,6 @@
                 </form>
                 <div style="clear:both;margin-bottom:30px;"></div>
                 <div id="homepage-categories" class="clearfix">
-                    <h3>Knowledge Base Categories</h3>
                 <?php
                 // Get homepage options
                 // Set category counter
@@ -40,17 +39,23 @@
                     'taxonomy' => 'incsub_wiki_category',
                     'pad_counts' => 1
                 );
-
+                
                 $st_categories = get_categories($st_hp_cat_args);
                 $st_categories = wp_list_filter($st_categories, array('parent' => 0));
                 // If there are catgegories
                 if ($st_categories) {
+                    $wikiSectionCount = 0;
                     foreach ($st_categories as $st_category) {
-                        echo '<h3> <a href="' . get_category_link($st_category->term_id) . '" title="' . sprintf(__('View all posts in %s', 'framework'), $st_category->name) . '" ' . '>' . $st_category->name . '</a>';
-                        //if (of_get_option('st_hp_cat_counts') == '1') {
-                            echo '<span class="cat-count">(' . $st_category->count . ')</span>';
-                        //}
+                        if($wikiSectionCount % 3 == 0) {
+                            if($wikiSectionCount != 0) 
+                                echo '</div><div class="wiki-section-group-gap" style="clear: both;"></div>';
+                            echo '<div class="wiki-section-group">';
+                        }
+                        echo '<div class="wiki-section">';
+                        echo '<h3> <a href="' . get_category_link($st_category->term_id) . '" title="' . sprintf(__('View all wikis in %s', 'framework'), $st_category->name) . '" ' . '>' . $st_category->name . '</a>';
+                        echo '<span class="cat-count"> (' . $st_category->count . ' Articles)</span>';
                         echo '</h3>';
+                        //$st_category->description //gets the description
 
                         // Sub category
                         $st_sub_category = get_category($st_category);
@@ -65,23 +70,73 @@
                         $st_sub_categories = wp_list_filter($st_sub_categories, array('parent' => $st_sub_category->cat_ID));
 
                         // If there are sub categories show them
-                        if ($st_sub_categories /*&& (of_get_option('st_hp_subcat') == 1)*/) {
+                        $subCatsFound = 0;
+                        if ($st_sub_categories) {
+                            $subCatsFound = 1;
                             foreach ($st_sub_categories as $st_sub_category) {
                                 ?>
                                 <ul class="sub-categories">
                                     <li>
                                         <h4><?php
                                             echo '<a href="' . get_category_link($st_sub_category->term_id) . '" title="' . sprintf(__('View all posts in %s', 'framework'), $st_sub_category->name) . '" ' . '>' . $st_sub_category->name . '</a>';
-                                            if (/*of_get_option('st_hp_subcat_counts') == '1'*/true) {
-                                                echo '<span class="cat-count">(' . $st_sub_category->count . ')</span>';
+                                            echo '<span class="cat-count"> (' . $st_sub_category->count . ' Articles)</span>';
+                                            ?>
+                                        </h4>
+                                        
+                                        <?php //Get child wikis for the sub category
+                                            $querystr = "
+                                                        SELECT $wpdb->posts.* , pageviews
+                                                        FROM $wpdb->posts
+                                                        LEFT OUTER JOIN wp_popularpostsdata ON wp_popularpostsdata.postid = $wpdb->posts.ID
+                                                        INNER JOIN wp_term_relationships ON (wp_posts.ID = wp_term_relationships.object_id)  
+                                                        WHERE 1=1 
+                                                            AND wp_term_relationships.term_taxonomy_id IN (".$st_sub_category->cat_ID.")
+                                                            AND $wpdb->posts.post_type = 'incsub_wiki'
+                                                        ORDER BY pageviews DESC
+                                                        LIMIT 2";
+
+                                                     $childcats = $wpdb->get_results($querystr, OBJECT);
+                                            echo '<ul class="wiki-list">';
+                                            foreach($childcats as $childwiki) {
+                                                echo '<li> <a class="wiki-section-link" href="'.get_permalink($childwiki->ID).'">'.$childwiki->post_title.'<span class="cat-count"> (' . $childwiki->pageviews . ' views)</span></a></li>';
                                             }
-                                            ?></h4>
+                                            echo '</ul>';?>
                                     </li>
                                 </ul>
                             <?php
                             }
                         }
-                    }
+                        
+                        $numPosts = 5;
+                        if($subCatsFound)
+                            $numPosts = 3;
+                        //Display the wikis under the main category
+                        $querystr = "
+                            SELECT $wpdb->posts.* , pageviews
+                            FROM $wpdb->posts
+                            LEFT OUTER JOIN wp_popularpostsdata ON wp_popularpostsdata.postid = $wpdb->posts.ID
+                            INNER JOIN wp_term_relationships ON (wp_posts.ID = wp_term_relationships.object_id)  
+                            WHERE 1=1 
+                                AND wp_term_relationships.term_taxonomy_id IN (".$st_category->cat_ID.")
+                                AND $wpdb->posts.post_type = 'incsub_wiki'
+                            ORDER BY pageviews DESC
+                            LIMIT $numPosts";
+
+                         $childcats = $wpdb->get_results($querystr, OBJECT);
+
+
+                        echo '<ul class="wiki-list">';
+                        foreach($childcats as $childwiki) {
+                            //Make sure it is not a sub category
+                            $catCheck = get_the_terms($childwiki->ID, 'incsub_wiki_category');
+                            if($catCheck[0]->term_id == $st_category->cat_ID)
+                                echo '<li> <a class="wiki-section-link" href="'.get_permalink($childwiki->ID).'">'.$childwiki->post_title.'<span class="cat-count"> (' . $childwiki->pageviews . ' views)</span></a>
+                                        </li>';
+                        }
+                        echo '</ul></div>';
+                        $wikiSectionCount++;
+                    } //End of the loop
+                    echo '</div><div style="clear: both; margin-bottom:200px;"></div>';
                 }
 				?>
 				</div>
