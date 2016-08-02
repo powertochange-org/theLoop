08e1824b1c40bc3d769cbe2c0ee849c236b6db43
brentn <nesbitt.brent@gmail.com>
2016-08-02 12:43:37 -0700

    remove cacheing code from myBalance, as the report now does cacheing

--- public_html/wp-content/themes/apps/financialreports/myBalance.php
+++ public_html/wp-content/themes/apps/financialreports/myBalance.php
@@ -1,7 +1,6 @@
 <?php 
 
 require('../../../../wp-blog-header.php');
-const KEY = 'staffAccountBalance';
 
 if (is_user_logged_in()) {
     include('rs_functions.php');
@@ -11,25 +10,19 @@ if (is_user_logged_in()) {
     $status = 204;
     $user_id = $current_user->user_login;
     if (sizeof($user_id) > 0 ) {
-        if (isset($_SESSION[KEY])) {
-            $result = $_SESSION[KEY];
+        $query = $wpdb->get_results($wpdb->prepare( 
+                "SELECT staff_account
+                FROM employee
+                WHERE user_login = %s", $user_id));
+        $staff_account = $query[0]->staff_account;
+        syslog(LOG_DEBUG, "Lookup up balance for ".$current_user->user_login.", account: ".$staff_account);
+        if (sizeof($staff_account)>0) {
+            $response = str_replace("<br />","",accountBalance($staff_account, $current_user->user_login));
+            syslog(LOG_INFO, "Account balance response: ".$response);
+            $account = explode("-",explode(":", $response)[1])[0];
+            $balance = explode(":", $response)[2];
+            $result = $account.": ".$balance;
             $status = 200;
-        } else {
-            $query = $wpdb->get_results($wpdb->prepare( 
-                    "SELECT staff_account
-                    FROM employee
-                    WHERE user_login = %s", $user_id));
-            $staff_account = $query[0]->staff_account;
-            syslog(LOG_DEBUG, "Lookup up balance for ".$current_user->user_login.", account: ".$staff_account);
-            if (sizeof($staff_account)>0) {
-                $response = str_replace("<br />","",accountBalance($staff_account, $current_user->user_login));
-                syslog(LOG_INFO, "Account balance response: ".$response);
-                $account = explode("-",explode(":", $response)[1])[0];
-                $balance = explode(":", $response)[2];
-                $result = $account.": ".$balance;
-                $_SESSION[KEY] = $result;
-                $status = 200;
-            }
         }
     }
     http_response_code($status);
