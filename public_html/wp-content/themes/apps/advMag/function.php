5ea1cbcb30129c2f4aa7c8a6f3d6828525314f45
matthew-chell <matthew.chell@p2c.com>
2017-05-04 14:38:16 -0700

    fixed typo

--- public_html/wp-content/themes/apps/advMag/function.php
+++ public_html/wp-content/themes/apps/advMag/function.php
@@ -56,7 +56,7 @@ class AdvMag{
 		//limit who ie only CS
 		$pc = getFieldEmployee('staff_account');
 		$pc = '100400';
-		if('' == $pc || is_null($pc) || ('8' != $pc && '9' != $pc)){
+		if('' == $pc || is_null($pc) || ('8' != $pc[0] && '9' != $pc[0])){
 			return array();
 		}
 		//https=>//seapi.powertochange.org/accounts?advancedFind=%7B%22table%22%3A%22A01%22%2C%22isDistinct%22%3Atrue%2C%22selectionCriteria%22%3A%5B%7B%22group%22%3A1%2C%22table%22%3A%22T04%22%2C%22operator%22%3A%22EQ%22%2C%22field%22%3A%22ProjectCode%22%2C%22isJoin%22%3Afalse%2C%22value%22%3A%22810550%22%7D%2C%7B%22isJoin%22%3Atrue%2C%22table%22%3A%22A01%22%2C%22value%22%3A%22T01%22%2C%22field%22%3Anull%2C%22operator%22%3Anull%7D%2C%7B%22isJoin%22%3Atrue%2C%22table%22%3A%22T01%22%2C%22field%22%3Anull%2C%22operator%22%3Anull%2C%22value%22%3A%22T04%22%7D%2C%7B%22group%22%3A1%2C%22table%22%3A%22T01%22%2C%22operator%22%3A%22ON_OR_BEFORE%22%2C%22field%22%3A%22Date%22%2C%22isJoin%22%3Afalse%2C%22value%22%3A%222016-03-15%22%7D%5D%2C%22viewId%22%3Anull%7D&offset=0&limit=9
