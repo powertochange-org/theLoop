ffaaa2f1f6fdba4213aa96febae93f0bd771ae93
matthew-chell <matthew.chell@p2c.com>
2017-10-16 09:16:55 -0700

    typo

--- public_html/wp-content/themes/apps/staffdirectory/givingpage-backend.php
+++ public_html/wp-content/themes/apps/staffdirectory/givingpage-backend.php
@@ -133,8 +133,8 @@ class Givingpage{
 			}
 			
 			$data['Description'] = array(
-				'@cdata' => '<ml><locale name="en-US">'.WebService::xmlEncodeSpecial(strip_tags($_POST['des'])).'</locale>'.
-					'<locale name="fr-CA">'.WebService::xmlEncodeSpecial(strip_tags($_POST['desFre'])).'</locale></ml>'
+				'@cdata' => '<ml><locale name="en-US">'.SO_API::xmlEncodeSpecial(strip_tags($_POST['des'])).'</locale>'.
+					'<locale name="fr-CA">'.SO_API::xmlEncodeSpecial(strip_tags($_POST['desFre'])).'</locale></ml>'
 			);
 		}
 		
