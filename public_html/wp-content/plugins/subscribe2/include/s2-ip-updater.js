77b16e7312d3316f4c07a59abd073860f934cbe4
Matt Chell <matthew.chell@p2c.com>
2019-02-11 15:03:45 -0800

    Updating plugins:
    Classic Editor
    iframe
    Subscribe2
    Subscribe to Comments Reloaded

--- public_html/wp-content/plugins/subscribe2/include/s2-ip-updater.js
+++ public_html/wp-content/plugins/subscribe2/include/s2-ip-updater.js
@@ -1,8 +1,11 @@
+/* exported getip */
 // Version 1.0 - original version
+// Version 1.1 - eslinted and fixed for Widget form name change
 
 function getip( json ) {
-	if ( true === document.body.contains( document.forms['s2form'] ) ) {
-		var ip = document.getElementsByName( 'ip' );
+	var ip, i;
+	if ( true === document.body.contains( document.forms.s2form ) || true === document.body.contains( document.forms.s2formwidget ) ) {
+		ip = document.getElementsByName( 'ip' );
 		for ( i = 0; i < ip.length; i++ ) {
 			if ( 's2form' === ip[i].parentElement.name ) {
 				ip[i].value = json.ip;
